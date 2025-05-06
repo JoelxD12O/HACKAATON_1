998a8c51c6b7862661f55e05d797493cb89b353a
Joelxd <joel.cayllahua@utec.edu.pe>
2025-05-06 16:52:01 -0500

    nuevo inicio gg

---
+++ src/main/java/Entidades/Empresa.java
@@ -0,0 +1,34 @@
+package Entidades;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String ruc;
+
+    private LocalDate fechaAfiliacion;
+
+    private boolean activa;
+
+    @OneToMany(mappedBy = "empresa", cascade = CascadeType.ALL)
+    private List<Usuario> usuarios;
+
+    @OneToMany(mappedBy = "empresa", cascade = CascadeType.ALL)
+    private List<RestriccionEmpresa> restricciones;
+}
---
+++ src/main/java/Entidades/Usuario.java
@@ -0,0 +1,41 @@
+package Entidades;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombreCompleto;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Rol rol;
+
+    @ManyToOne
+    @JoinColumn(name = "empresa_id")
+    private Empresa empresa;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private List<LimiteUsuario> limites;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private List<SolicitudIA> solicitudes;
+}
