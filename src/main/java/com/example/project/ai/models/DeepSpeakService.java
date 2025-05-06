357806c768d717fa2ad5200410675dff27da41ae
MiguelPB <miguel.paucar@utec.edu.pe>
2025-05-06 17:35:33 -0500

    agregando deepspeakservice

--- src/main/java/com/example/project/ai/models/DeepSpeakService.java
+++ src/main/java/com/example/project/ai/models/DeepSpeakService.java
@@ -1,4 +1,18 @@
 package com.example.project.ai.models;
+import org.springframework.stereotype.Service;
+import ai.dto.AIRequestDTO;
 
-public class DeepSpeakService {
+@Service
+public class DeepSpeakService implements IAiModelService{
+
+    @Override
+    public String processPromt(AIRequestDTO request){
+        return "Respuesta simulada de DeepSeek para: " + request.getPrompt();
+    }
+
+
+    @Override
+    private String getModelName(){
+        return "DeepSeek";
+    }
 }
